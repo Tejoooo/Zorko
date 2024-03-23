fde4a1d0cfcd72b79c78fc70f3ab79c60360d726
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-03-23 16:08:45 +0530

    nav modified

--- lib/pages/appHome.dart
+++ lib/pages/appHome.dart
@@ -13,7 +13,6 @@ class AppHome extends StatefulWidget {
 }
 
 class _AppHomeState extends State<AppHome> {
-
   var _currentIndex = 0;
 
   late final List<Widget> _pages;
@@ -23,6 +22,7 @@ class _AppHomeState extends State<AppHome> {
       _currentIndex = val;
     });
   }
+
   @override
   void initState() {
     super.initState();
@@ -42,77 +42,80 @@ class _AppHomeState extends State<AppHome> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async {
-        if (_currentIndex != 0) {
-          setState(() {
-            _currentIndex = 0;
-          });
-          return Future(() => false);
-        }
-        bool res = await showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Warning'),
-            content: const Text('Do you really want to exit'),
-            actions: [
-              TextButton(
-                child: const Text('Yes'),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-              TextButton(
-                child: const Text('No'),
-                onPressed: () => Navigator.pop(context, false),
-              ),
-            ],
+        onWillPop: () async {
+          if (_currentIndex != 0) {
+            setState(() {
+              _currentIndex = 0;
+            });
+            return Future(() => false);
+          }
+          bool res = await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Warning'),
+              content: const Text('Do you really want to exit'),
+              actions: [
+                TextButton(
+                  child: const Text('Yes'),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+                TextButton(
+                  child: const Text('No'),
+                  onPressed: () => Navigator.pop(context, false),
+                ),
+              ],
+            ),
+          );
+          return Future.value(res);
+        },
+        child: Scaffold(
+          body: _pages[_currentIndex],
+          bottomNavigationBar: Container(
+            // height: 66, 
+            padding: const EdgeInsets.all(9),
+            // margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 22),
+            decoration: BoxDecoration(
+              // borderRadius: const BorderRadius.all(Radius.circular(24)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 20,
+                  offset: Offset(0, -3),
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.home,
+                      color: _currentIndex == 0 ? Colors.blue : Colors.grey),
+                  onPressed: () => _onItemTapped(0),
+                ),
+                IconButton(
+                  icon: Icon(Icons.favorite,
+                      color: _currentIndex == 1 ? Colors.blue : Colors.grey),
+                  onPressed: () => _onItemTapped(1),
+                ),
+                IconButton(
+                  icon: Icon(Icons.grid_on,
+                      color: _currentIndex == 2 ? Colors.blue : Colors.grey),
+                  onPressed: () => _onItemTapped(2),
+                ),
+                IconButton(
+                  icon: Icon(Icons.shopping_cart,
+                      color: _currentIndex == 3 ? Colors.blue : Colors.grey),
+                  onPressed: () => _onItemTapped(3),
+                ),
+                IconButton(
+                  icon: Icon(Icons.person,
+                      color: _currentIndex == 4 ? Colors.blue : Colors.grey),
+                  onPressed: () => _onItemTapped(4),
+                ),
+              ],
+            ),
           ),
-        );
-        return Future.value(res);
-      },
-      child: Scaffold(
-        body: _pages[_currentIndex],
-      bottomNavigationBar: Container(
-      height: 66, // Adjust the height according to your design
-      padding: const EdgeInsets.all(9),
-    margin: const EdgeInsets.symmetric(horizontal: 12,vertical: 22),
-      decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(24)),
-
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.3),
-            blurRadius: 20,
-            offset: Offset(0, -3),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.home, color: _currentIndex == 0 ? Colors.blue : Colors.grey),
-            onPressed: () => _onItemTapped(0),
-          ),
-          IconButton(
-            icon: Icon(Icons.favorite, color: _currentIndex == 1 ? Colors.blue : Colors.grey),
-            onPressed: () => _onItemTapped(1),
-          ),
-          IconButton(
-            icon: Icon(Icons.grid_on, color: _currentIndex == 2 ? Colors.blue : Colors.grey),
-            onPressed: () => _onItemTapped(2),
-          ),
-          IconButton(
-            icon: Icon(Icons.shopping_cart, color: _currentIndex == 3 ? Colors.blue : Colors.grey),
-            onPressed: () => _onItemTapped(3),
-          ),
-          IconButton(
-            icon: Icon(Icons.person, color: _currentIndex == 4 ? Colors.blue : Colors.grey),
-            onPressed: () => _onItemTapped(4),
-          ),
-        ],
-      ),
-    ),
-      )
-    );
+        ));
   }
 }
