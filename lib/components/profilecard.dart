854ee9d85013f0b91b85c0af0543f09337382ec7
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-03-24 09:31:30 +0530

    update:fetching user details to profile

--- lib/components/profilecard.dart
+++ lib/components/profilecard.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class ProfileCard extends StatefulWidget {
   final String? name;
+  final String? word;
   const ProfileCard({
     Key? key,
     required this.name,
+    required this.word,
   }) : super(key: key);
 
   @override
@@ -29,8 +31,10 @@ class _ProfileCardState extends State<ProfileCard> {
               '${widget.name}',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-
-             Icon(Icons.arrow_right_alt_outlined),
+            Text(
+              '${widget.word}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
--- lib/pages/myprofile.dart
+++ lib/pages/myprofile.dart
@@ -1,5 +1,13 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings, unused_local_variable
+
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:zorko/components/profilecard.dart';
+import 'package:zorko/components/snackBar.dart';
+import 'package:zorko/constants.dart';
+import 'package:http/http.dart' as http;
 
 class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
@@ -9,10 +17,50 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
+
+  Map<String,dynamic>userData = {
+    'name':'Nithin',
+    'ph_no':'+917013313866',
+    'image':'images/dup.png',
+    'pincode':'515411',
+    'coins':50
+  };
+  bool isLoading = false;
+  
+  void _init() async{
+    setState(() {
+      isLoading = true;
+    });
+    User? user = await FirebaseAuth.instance.currentUser;
+    if (user !=null){
+      String uid = user.uid;
+      String apiURL = backendURL + "api/user/?userID="+ uid;
+      final response = await http.get(Uri.parse(apiURL));
+      if (response.statusCode == 200){
+        Map<String,dynamic> data = jsonDecode(response.body);
+        setState(() {
+          userData = data;
+          isLoading = false;
+        });
+      } else{
+        ErrorSnackBar(context, "Something went wrong!");
+      }
+    }
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _init();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
+    return Scaffold(
+          body: isLoading ? Center(child: CircularProgressIndicator()) : SingleChildScrollView(
+            child: Container(
               padding: EdgeInsets.all(20),
               child: Column(
                 children: [
@@ -30,44 +78,45 @@ class _MyProfileState extends State<MyProfile> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Image.asset('assets/h4.png',
+                        Image.network(backendURL + userData['profilepic'],
                         height: 100,
                         width: 100,
                         ),
-                        Divider(
-                          color: Colors.black,
-                          height: 100,
-                          thickness: 2,
-                          indent: 20,
-                          endIndent: 20,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(color: Colors.white,width: 10,child: Text('Deepti',style: TextStyle(fontSize: 20),),), 
-                            SizedBox(height: 5,),
-                            Container(color: Colors.white,width: 10,child: Text('deepti@gmail.com',style: TextStyle(fontSize: 20),),), 
-                            SizedBox(height: 5,),
-                            Container(color: Colors.white,width: 10,child: Text('+91987654321',style: TextStyle(fontSize: 20),),), 
-                          ],
-                        ),
-          
+                        // Divider(
+                        //   color: Colors.black,
+                        //   height: 100,
+                        //   thickness: 2,
+                        //   indent: 20,
+                        //   endIndent: 20,
+                        // ),
+                        // Column(
+                        //   crossAxisAlignment: CrossAxisAlignment.start,
+                        //   mainAxisAlignment: MainAxisAlignment.center,
+                        //   children: [
+                        //     Container(color: Colors.white,child: Text('Deepti',style: TextStyle(fontSize: 20),),), 
+                        //     SizedBox(height: 5,),
+                        //     Container(color: Colors.white,child: Text('deepti@gmail.com',style: TextStyle(fontSize: 20),),), 
+                        //     SizedBox(height: 5,),
+                        //     Container(color: Colors.white,child: Text('+91987654321',style: TextStyle(fontSize: 20),),), 
+                        //   ],
+                        // ),
+                    
                       ],
                     ),
                   ),
                   SizedBox(height: 20),
-                  ProfileCard(name: 'ORDERS'),
+                  ProfileCard(name: 'Name',word: userData['name'],),
                   SizedBox(height: 20),
-                  ProfileCard(name: 'REVIEWS'),
+                  ProfileCard(name: 'Phone',word: userData['ph_no'],),
                   SizedBox(height: 20),
-                  ProfileCard(name: 'ORDERS'),
+                  ProfileCard(name: 'pincode',word: userData['pincode'],),
                   SizedBox(height: 20),
-                  ProfileCard(name: 'REVIEWS'),
+                  ProfileCard(name: 'Coins',word: '10',),
                   SizedBox(height: 20),
                 ],
               ),
-      ),
+            ),
+          ) 
     );
 }
 }
